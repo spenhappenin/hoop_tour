18720b1243578251efb17ce61f07b66161fddc12
Spencer Richards <spencer.richards7@gmail.com>
2017-04-10 23:58:28 -0600

    FAQ - layout changed to collapsibles

--- client/components/FAQ.js
+++ client/components/FAQ.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Collapsible from 'react-collapsible';
 
 class FAQ extends React.Component {
+
+	componentDidMount() {
+		$('.collapsible').collapsible();
+	}
+
+	componentDidUpdate() {
+		$('.collapsible').collapsible();
+	}
+
 	render() {
+
 		return(
 			<div className='container'>
 
@@ -13,59 +24,81 @@ class FAQ extends React.Component {
 					<h2>FAQ'S</h2>
 				</div>
 
-				<div>
-					<h3>How do I play in a Hoop Tour USA event?</h3>
-					<ul>
-						<li>Step 1: Grab 2-3 friends who can ball</li>
-						<li>Step 2: Register your team</li>
-						<li>Step 3: Experience Hoop Tour USA in your area!</li>
-					</ul>
-				</div>
+				<ul className="collapsible" data-collapsible="accordion">
+			    <li>
+			      <div className="collapsible-header grey lighten-4">How do I play in a Hoop Tour USA event?</div>
+			      <div className="collapsible-body">
+					    <ul>
+								<li>Step 1: Grab 2-3 friends who can ball</li>
+								<li>Step 2: Register your team</li>
+								<li>Step 3: Experience Hoop Tour USA in your area!</li>
+							</ul>
+			      </div>
+			    </li>
+			  </ul>
 
-				<div>
-					<h3>How do I register?</h3>
-					<ul>
-						<li>Step 1: CLick on registration button underneath whichever hoop tour location you are interested in playing!</li>
-						<li>Step 2: Create a Sport Ngin account (it only takes a minute or two)!</li>
-						<li>Step 3: ETeam captain is in charge of team registration.  There is a one-time team payment.  Team captain will have the option of registering his/her whole team now, or waiting.  Make sure not to wait too long, secure your spot now and find a team later if you have to!</li>
-						<li>Step 4: Ball out and enjoy Hoop Tour USA, a basketball event that unites communities together through the game of basketball!</li>
-					</ul>
-				</div>
+			  <ul className="collapsible" data-collapsible="accordion">
+			    <li>
+			      <div className="collapsible-header grey lighten-4">How do I register?</div>
+			      <div className="collapsible-body">
+							<ul>
+								<li>Step 1: CLick on registration button underneath whichever hoop tour location you are interested in playing!</li>
+								<li>Step 2: Create a Sport Ngin account (it only takes a minute or two)!</li>
+								<li>Step 3: ETeam captain is in charge of team registration.  There is a one-time team payment.  Team captain will have the option of registering his/her whole team now, or waiting.  Make sure not to wait too long, secure your spot now and find a team later if you have to!</li>
+								<li>Step 4: Ball out and enjoy Hoop Tour USA, a basketball event that unites communities together through the game of basketball!</li>
+							</ul>
+			      </div>
+			    </li>
+			  </ul>
 
-				<div>
-					<h3>What is included in my registration?</h3>
-					<ul>
-						<span>Everyone receives an exclusive Hoop Tour USA T-Shirt and a Hoop Tour USA drawstring backpack.  Each team is guaranteed 4 games! Most importantly, you get to play the game you love while enjoying awesome music, delicious food, and soaking in a fun community event!  There are also awesome prizes for our winners!</span>
-					</ul>
-				</div>
+			  <ul className="collapsible" data-collapsible="accordion">
+			    <li>
+			      <div className="collapsible-header grey lighten-4">What is included in my registration?</div>
+			      <div className="collapsible-body">
+			      	<span>Everyone receives an exclusive Hoop Tour USA T-Shirt and a Hoop Tour USA drawstring backpack.  Each team is guaranteed 4 games! Most importantly, you get to play the game you love while enjoying awesome music, delicious food, and soaking in a fun community event!  There are also awesome prizes for our winners!</span>
+			      </div>
+			    </li>
+			  </ul>
 
-				<div>
-					<h3>How much is the team entry fee?</h3>
-					<ul>
-						<span>Team registration fees are $99/team during regular registration.  Once regular registration ends, prices increase.  Make sure to stay tuned with our social media accounts for constant promo codes.  We want to make this an affordable event that is more than worth your registration fee!</span>
-					</ul>
-				</div>
+			  <ul className="collapsible" data-collapsible="accordion">
+			    <li>
+			      <div className="collapsible-header grey lighten-4">How much is the team entry fee?</div>
+			      <div className="collapsible-body">
+			      	<span>Team registration fees are $99/team during regular registration.  Once regular registration ends, prices increase.  Make sure to stay tuned with our social media accounts for constant promo codes.  We want to make this an affordable event that is more than worth your registration fee!</span>
+			      </div>
+			    </li>
+			  </ul>
 
-				<div>
-					<h3>How do I know which grade level my child should be registered in?</h3>
-					<ul>
-						<span>Register your child in the grade they will be in during the fall. Example, if they just finished 4th grade, you would register them in the 5-6th grade division.</span>
-					</ul>
-				</div>
+			 	<ul className="collapsible" data-collapsible="accordion">
+			    <li>
+			      <div className="collapsible-header grey lighten-4">How do I know which grade level my child should be registered in?</div>
+			      <div className="collapsible-body">
+			      	<span>Register your child in the grade they will be in during the fall. Example, if they just finished 4th grade, you would register them in the 5-6th grade division</span>
+			      </div>
+			    </li>
+			  </ul>
 
-				<div>
-					<h3>Is there any way to combine boys/girls teams?</h3>
-					<ul>
-						<span>Co-ed teams are allowed, however, they must compete in the male division.  If we get enough requests, we are open to creating a co-ed division.</span>
-					</ul>
-				</div>
+			 	<ul className="collapsible" data-collapsible="accordion">
+			    <li>
+			      <div className="collapsible-header grey lighten-4">Is there any way to combine boys/girls teams?</div>
+			      <div className="collapsible-body">
+			      	<span>Register your child in the grade they will be in during the fall. Example, if they just finished 4th grade, you would register them in the 5-6th grade division</span>
+			      </div>
+			    </li>
+			  </ul>
 
-				<div>
-					<h3>Am I allowed to play up?</h3>
-					<ul>
-						<span>You are allowed to play up in divisions, but never down.  Meaning, if you are in the 4th grade, but want to play with your friends who are 5th graders, that is allowed, however, your team will compete in the 5th grade division. </span>
-					</ul>
-				</div>
+			  <ul className="collapsible" data-collapsible="accordion">
+			    <li>
+			      <div className="collapsible-header grey lighten-4">Am I allowed to play up?</div>
+			      <div className="collapsible-body">
+			      	<span>You are allowed to play up in divisions, but never down.  Meaning, if you are in the 4th grade, but want to play with your friends who are 5th graders, that is allowed, however, your team will compete in the 5th grade division.</span>
+			      </div>
+			    </li>
+			  </ul>
+
+			  <br />
+			  <br />
+			  <br />
 
 			</div>
 		)
--- client/components/Rules.js
+++ client/components/Rules.js
@@ -3,8 +3,47 @@ import React from 'react';
 class Rules extends React.Component {
 	render() {
 		return(
-			<div>
-				Rules
+			<div className='container'>
+
+				<br />
+				<br />
+
+				<div>
+					<h2>Hoop Tour USA Rules</h2>
+				</div>
+
+				<div>
+					<ol>
+						<li>Games are 20 minutes long, or first to 21 points straight up. (There is no win by two)</li>
+						<li>Games are scored by 1’s and 2’s</li>
+							<ul>
+								<li>a. 1 point is awarded if a basket is made within the 3 point line</li>
+								<li>b. 2 points are awarded if a basket is made behind the 3 point line</li>
+							</ul>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ol>
+				</div>
+
 			</div>
 		)
 	}
