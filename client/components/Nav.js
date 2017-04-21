24d7e7ad84a4dbc59fa858721603d84fa59fc18a
Spencer Richards <spencer.richards7@gmail.com>
2017-04-21 17:25:40 -0600

    side nav started

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class Nav extends React.Component {
+
+	componentDidMount() {
+	  $(".button-collapse").sideNav();
+	}
+
+	componentWillUpdate() {
+	  $(".button-collapse").sideNav();
+	}
+
 	render() {
 		return(
 			<div className='navbar-fixed'>
 				<nav>
 			    <div className="nav-wrapper color-black nav-txt">
 			      <Link to="/home" className='nav-logo'><img src='http://res.cloudinary.com/dtb6lx1s4/image/upload/v1491865993/hoop_tour_logo_tc6szb.png' /></Link>
+			      <a href='' data-activates='mobile' className='button-collapse'>
+              <i className='medium material-icons nav-tabs'>menu</i>
+            </a>
 			      <ul id="nav-mobile" className="right hide-on-med-and-down">
 			        <li><Link className='nav-item' to={ '/home' }>HOME</Link></li>
 			        <li><Link className='nav-item' to={ '/about' }>ABOUT</Link></li>
@@ -17,6 +29,16 @@ class Nav extends React.Component {
 			        <li><Link className='nav-item' to={ '/FAQ' }>FAQ'S</Link></li>
 			        <li><Link className='nav-item' to={ '/rules' }>RULES</Link></li>
 			      </ul>
+			      <ul className="side-nav" id="mobile">
+              <i className="material-icons"></i>
+				        <li><Link className='nav-item' to={ '/home' }>HOME</Link></li>
+				        <li><Link className='nav-item' to={ '/about' }>ABOUT</Link></li>
+				        <li><Link className='nav-item' to={ '/locations' }>LOCATIONS</Link></li>
+				        <li><Link className='nav-item' to={ '/sponsorship' }>SPONSORSHIP OPPORTUNITIES</Link></li>
+				        <li><Link className='nav-item' to={ '/contact' }>CONTACT</Link></li>
+				        <li><Link className='nav-item' to={ '/FAQ' }>FAQ'S</Link></li>
+				        <li><Link className='nav-item' to={ '/rules' }>RULES</Link></li>
+            </ul>
 			    </div>
 			  </nav> 
 		  </div>  
