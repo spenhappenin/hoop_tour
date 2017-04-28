952279bfb806b6e6e27bbc6a25d13ef3fb0341c3
Spencer Richards <spencer.richards7@gmail.com>
2017-04-27 18:02:09 -0600

    ham menu moves left on mobile

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -4,23 +4,17 @@ import { Link } from 'react-router';
 class Nav extends React.Component {
 
 	componentDidMount() {
-	  $(".button-collapse").sideNav();
-	}
-
-	componentWillUpdate() {
-	  $(".button-collapse").sideNav();
+	  $(".button-collapse").sideNav({
+	  	closeOnClick: true
+	  });
 	}
 
 	render() {
 		return(
-			<div className='navbar-fixed'>
-				<nav>
-			    <div className="nav-wrapper color-black nav-txt">
-			      <Link to="/home" className='nav-logo'><img src='http://res.cloudinary.com/dtb6lx1s4/image/upload/v1491865993/hoop_tour_logo_tc6szb.png' /></Link>
-			      <a href='' data-activates='mobile' className='button-collapse'>
-              <i className='medium material-icons nav-tabs'>menu</i>
-            </a>
-			      <ul id="nav-mobile" className="right hide-on-med-and-down">
+			<div>
+				<div>
+		      <ul className="side-nav" id="mobile">
+            <i className="material-icons"></i>
 			        <li><Link className='nav-item' to={ '/home' }>HOME</Link></li>
 			        <li><Link className='nav-item' to={ '/about' }>ABOUT</Link></li>
 			        <li><Link className='nav-item' to={ '/locations' }>LOCATIONS</Link></li>
@@ -28,9 +22,16 @@ class Nav extends React.Component {
 			        <li><Link className='nav-item' to={ '/contact' }>CONTACT</Link></li>
 			        <li><Link className='nav-item' to={ '/FAQ' }>FAQ'S</Link></li>
 			        <li><Link className='nav-item' to={ '/rules' }>RULES</Link></li>
-			      </ul>
-			      <ul className="side-nav" id="mobile">
-              <i className="material-icons"></i>
+          </ul>
+				</div>
+				<div className='navbar-fixed'>
+					<nav>
+				    <div className="nav-wrapper color-black nav-txt">
+				      <Link to="/home" className='nav-logo'><img src='http://res.cloudinary.com/dtb6lx1s4/image/upload/v1491865993/hoop_tour_logo_tc6szb.png' /></Link>
+				      <a href='' data-activates='mobile' className='button-collapse ham-icon'>
+	              <i className='medium material-icons nav-tabs'>menu</i>
+	            </a>
+				      <ul id="nav-mobile" className="right hide-on-med-and-down">
 				        <li><Link className='nav-item' to={ '/home' }>HOME</Link></li>
 				        <li><Link className='nav-item' to={ '/about' }>ABOUT</Link></li>
 				        <li><Link className='nav-item' to={ '/locations' }>LOCATIONS</Link></li>
@@ -38,9 +39,10 @@ class Nav extends React.Component {
 				        <li><Link className='nav-item' to={ '/contact' }>CONTACT</Link></li>
 				        <li><Link className='nav-item' to={ '/FAQ' }>FAQ'S</Link></li>
 				        <li><Link className='nav-item' to={ '/rules' }>RULES</Link></li>
-            </ul>
-			    </div>
-			  </nav> 
+				      </ul>
+				    </div>
+				  </nav> 
+			  </div>
 		  </div>  
 		)
 	}
