4d9ad50c505871aa3cc2db06b6ad1660b122ed06
Spencer Richards <spencer.richards7@gmail.com>
2017-05-18 18:24:06 -0600

    bug fix - everything changed back

--- client/components/Home.js
+++ client/components/Home.js
@@ -12,34 +12,35 @@ class Home extends React.Component {
 	render() {
 		return(
 			<div>
+				<div className='main-header center'>
+					<div className='overlay'>
+						<div className='flex-col v-center h-center'>
+							<div className=''>
+								<br />
+								<img className='responsive-img' src='http://res.cloudinary.com/dtb6lx1s4/image/upload/v1492487790/hoop_tour_white_text_zgdrwx.png' />
+							</div>
 
-				<div className='color-red main-header center'>
+							<div>
+								<h2 className='foo'>#UniteTheCity</h2>
+							</div>
 
-					<div className=''>
-						<br />
-						<img className='responsive-img' src='https://res.cloudinary.com/omash612/image/upload/c_scale,w_2131/v1483683226/artsyBasketball_ysrsox.jpg' />
+							<br />
+
+							<br />
+
+							<div className=''>
+								<Link to={'/locations'} className='btn color-blue'>Register!</Link>
+							</div>
+
+							<br />
+							<br />
+							<br />
+
+							<div>
+								<img className='lifetime-header' src='https://www.lifetime.com/content/images/lifetimelogo-w-tag_black.png' />
+							</div>
+						</div>
 					</div>
-
-					<br />
-
-					<div>
-						<h2 className='foo'>#UniteTheCity</h2>
-					</div>
-
-					<br />
-
-					<div className=''>
-						<Link to={'/locations'} className='btn color-blue'>Register!</Link>
-					</div>
-
-					<br />
-					<br />
-					<br />
-
-					<div>
-						<img className='lifetime-header' src='https://www.lifetime.com/content/images/lifetimelogo-w-tag_black.png' />
-					</div>
-
 				</div>
 
 				<br />
@@ -149,4 +150,4 @@ class Home extends React.Component {
 	}
 }
 
-export default Home
+export default Home;
